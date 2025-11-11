1ac2361aab07eff2290b42bd887a8053076df721
Alan Villalobos <145631906+SanicSpeed1238@users.noreply.github.com>
2025-11-10 22:45:52 -0600

    created default functions for light, heavy, cold, and hot potion effects on objects

--- Assets/_Abstract Alchemy/Scripts/PotionTargetDefault.cs
+++ Assets/_Abstract Alchemy/Scripts/PotionTargetDefault.cs
@@ -1,63 +1,126 @@
-// Used on Generic Objects
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class PotionTargetDefault : PotionTargetAbstract
 {
+    [Header("Size Effect")]
+    [Range(0.1f, 1f)]
+    public float shrinkMultiplier = 0.5f;
+    [Range(1f, 3f)]
+    public float growMultiplier = 2f;
+
+    [Header("Weight Effect")]
+    [Range(1f, 3f)]
+    public float lightMultiplier = 2f;
+    [Range(0.1f, 1f)]
+    public float heavyMultiplier = 0.5f;
+
+    [Header("Temperature Effect")]
+    [Range(0f, 0.5f)]
+    public float coldMultiplier = 0.2f;
+    private readonly float defaultFrictionValue = 0.8f;
+    [Range(0.1f, 1f)]
+    public float hotMultiplier = 0.5f;
+    private readonly float defaultBouncinessValue = 0f;
+
     protected override void OnPotionEffectsAdded(PotionEffects.Effects effects)
     {
         if (effects.HasFlag(PotionEffects.Effects.Shrink))
         {
-            transform.localScale *= 0.5f;
+            transform.localScale *= shrinkMultiplier;
         }
         if (effects.HasFlag(PotionEffects.Effects.Grow))
         {
-            transform.localScale *= 2f;
+            transform.localScale *= growMultiplier;
         }
 
         if (effects.HasFlag(PotionEffects.Effects.Light))
         {
-            // Light Effect
+            if (TryGetComponent<XRGrabInteractable>(out var interactor))
+            {
+                interactor.throwVelocityScale *= lightMultiplier;
+            }
         }
         if (effects.HasFlag(PotionEffects.Effects.Heavy))
         {
-            // Heavy Effect
+            if (TryGetComponent<XRGrabInteractable>(out var interactor))
+            {
+                interactor.throwVelocityScale *= heavyMultiplier;
+            }
         }
 
         if (effects.HasFlag(PotionEffects.Effects.Cold))
         {
-            // Cold Effect
+            if (TryGetComponent<Collider>(out var collider))
+            {
+                if (collider.material != null)
+                {
+                    PhysicMaterial colliderMat = collider.material;
+                    colliderMat.staticFriction = coldMultiplier;
+                    colliderMat.dynamicFriction = coldMultiplier;
+                }
+            }
         }
         if (effects.HasFlag(PotionEffects.Effects.Hot))
         {
-            // Hot Effect
+            if (TryGetComponent<Collider>(out var collider))
+            {
+                if (collider.material != null)
+                {
+                    PhysicMaterial colliderMat = collider.material;
+                    colliderMat.bounciness = hotMultiplier;
+                }
+            }
         }
     }
     protected override void OnPotionEffectsRemoved(PotionEffects.Effects effects)
     {
         if (effects.HasFlag(PotionEffects.Effects.Shrink))
         {
-            transform.localScale *= 2f;
+            transform.localScale /= shrinkMultiplier;
         }
         if (effects.HasFlag(PotionEffects.Effects.Grow))
         {
-            transform.localScale *= 0.5f;
+            transform.localScale /= growMultiplier;
         }
 
         if (effects.HasFlag(PotionEffects.Effects.Light))
         {
-            // Light Effect
+            if (TryGetComponent<XRGrabInteractable>(out var interactor))
+            {
+                interactor.throwVelocityScale /= lightMultiplier;
+            }
         }
         if (effects.HasFlag(PotionEffects.Effects.Heavy))
         {
-            // Heavy Effect
+            if (TryGetComponent<XRGrabInteractable>(out var interactor))
+            {
+                interactor.throwVelocityScale /= heavyMultiplier;
+            }
         }
 
         if (effects.HasFlag(PotionEffects.Effects.Cold))
         {
-            // Cold Effect
+            if (TryGetComponent<Collider>(out var collider))
+            {
+                if (collider.material != null)
+                {
+                    PhysicMaterial colliderMat = collider.material;
+                    colliderMat.staticFriction = defaultFrictionValue;
+                    colliderMat.dynamicFriction = defaultFrictionValue;
+                }
+            }
         }
         if (effects.HasFlag(PotionEffects.Effects.Hot))
         {
-            // Hot Effect
+            if (TryGetComponent<Collider>(out var collider))
+            {
+                if (collider.material != null)
+                {
+                    PhysicMaterial colliderMat = collider.material;
+                    colliderMat.bounciness = defaultBouncinessValue;
+                }
+            }
         }
     }
 }
