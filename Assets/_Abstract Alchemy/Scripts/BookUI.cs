251a4c77a6064f852bda8862c3b3804a1c340ad5
LuminusPrime12 <ethanhull6849@gmail.com>
2025-12-06 23:38:08 -0600

    Image
    Just added the image. I'm still working on a lot in regards to the physical button presses. Let me know when the other images are done and post them to the discord, or upload them and notify me so I know where to find them in unity.

--- Assets/_Abstract Alchemy/Scripts/BookUI.cs
+++ Assets/_Abstract Alchemy/Scripts/BookUI.cs
@@ -5,22 +5,19 @@ using UnityEngine.UI;
 public class BookUI : MonoBehaviour
 {
     [Header("UI Elements")]
-    public TextMeshProUGUI pageText;
-    public Image logoImage;
+    public Image pageImage;            // Main image display
+    public TextMeshProUGUI pageText;   // Final text page
 
     public Button nextButton;
     public Button prevButton;
 
-    private int currentPage = 0;
+    [Header("Page Content")]
+    public Sprite[] imagePages;        // All pages that are images
+    [TextArea(3, 10)]
+    public string finalTextPage;       // Last page = text
 
-    // Page 0 will be the image.
-    // Pages 1+ will be text.
-    private string[] textPages = {
-        "Welcome to Abstract Alchemy! This book will teach you how to play.",
-        "Use the grip buttons to grab objects in the world.",
-        "Press the trigger to interact or use tools.",
-        "Collect all the items and mix them into different potions. Good luck!"
-    };
+    private int currentPage = 0;        // 0..(imagePages.Length) 
+                                        // Last page = text at index = imagePages.Length
 
     void Start()
     {
@@ -32,7 +29,7 @@ public class BookUI : MonoBehaviour
 
     void NextPage()
     {
-        if (currentPage < textPages.Length)  // logo is page 0, text pages go 1..N
+        if (currentPage < imagePages.Length)   // last index is the text page
         {
             currentPage++;
             UpdatePage();
@@ -50,26 +47,28 @@ public class BookUI : MonoBehaviour
 
     void UpdatePage()
     {
-        if (currentPage == 0)
+        // If we're still in the image range
+        if (currentPage < imagePages.Length)
         {
             // Show image page
-            logoImage.gameObject.SetActive(true);
+            pageImage.gameObject.SetActive(true);
             pageText.gameObject.SetActive(false);
 
-            prevButton.interactable = false;           // can't go back from logo
-            nextButton.interactable = true;            // can always go forward if text exists
+            pageImage.sprite = imagePages[currentPage];
+
+            prevButton.interactable = currentPage > 0;
+            nextButton.interactable = true;  // can go forward until final page
         }
         else
         {
-            // Show text page
-            logoImage.gameObject.SetActive(false);
+            // Final page = TEXT
+            pageImage.gameObject.SetActive(false);
             pageText.gameObject.SetActive(true);
 
-            pageText.text = textPages[currentPage - 1];  // subtract 1 because page 1 = textPages[0]
+            pageText.text = finalTextPage;
 
             prevButton.interactable = true;
-            nextButton.interactable = currentPage < textPages.Length;
+            nextButton.interactable = false; // No page after final text page
         }
     }
 }
-
